98022ce2170bdbd2a4d55d7385e8bc1eb52f5d87
april2nds <rlawnsgh8395@naver.com>
2022-09-14 09:18:40 +0900

    [UPDATE] dir

---
+++ day220913/src/back_db_ws_08_3_schema.sql
@@ -0,0 +1,104 @@
+-- Active: 1663060283928@@127.0.0.1@3306@world
+
+show tables;
+-- 1. 도시명 kabul이 속한 국가의 이름은?
+SELECT country.code, country.name
+FROM country, city
+WHERE country.Code = city.CountryCode
+and city.name = 'kabul';
+
+-- 2. 국가의 공식 언어 사용률이 100%인 국가의 정보를 출력하시오. 국가 이름으로 정렬( 오름차순 )
+
+SELECT a.Name, b.Language, b.Percentage
+FROM 
+    country a,
+    (
+        SELECT CountryCode, Language, Percentage
+        FROM countrylanguage
+        WHERE Percentage = 100
+        AND IsOfficial = 'T'
+    ) b
+WHERE a.Code = b.CountryCode
+ORDER BY a.Name;
+
+-- 3. 도시명 amsterdam에서 사용되는 주요 언어와 amsterdam이 속한 국가는?
+SELECT a.Name, b.Language, c.name
+FROM 
+    country a,
+    (
+        SELECT CountryCode, Language, max(Percentage)
+        FROM countrylanguage
+        GROUP BY CountryCode
+    ) b,
+    city c
+WHERE a.Code = b.CountryCode
+AND a.Code = c.CountryCode
+and c.Name = 'amsterdam';
+
+-- 4. 국가명이 united로 시작하는 국가의 정보와 수도의 이름, 인구를 함께 출력하시오 ( 단, 수도 정보가 없다면 출력하지 않는다 )
+SELECT a.Name, a.Capital, b.Name '수도', b.Population '수도인구'
+FROM country a, city b
+WHERE a.Capital = b.ID
+and a.Name like 'united%';
+
+-- 5. 국가명이 united로 시작하는 국가의 정보와 수도의 이름, 인구를 함께 출력하시오 ( 단, 수도 정보가 없다면 수도없음으로 출력한다. )
+SELECT a.Name, a.Capital, b.Name '수도', b.Population '수도인구'
+FROM country a, city b
+WHERE a.Capital = b.ID
+and a.Name like 'united%'
+UNION
+SELECT Name, Capital, '수도없음', '수도없음'
+FROM country
+WHERE 
+Name like 'united%'
+and Capital is null;
+
+-- 6. 국가 코드 che의 공식 언어 중 가장 사용률이 높은 언어보다 사용율이 높은 공식언어를 사용하는 국가는 몇 곳인가?
+SELECT count(*) '국가수'
+FROM (
+        SELECT CountryCode, max(Percentage)
+        FROM countrylanguage
+        WHERE IsOfficial = 'T'
+        GROUP BY CountryCode
+        HAVING max(Percentage) > (
+                                    select max(Percentage) 
+                                    from countrylanguage 
+                                    where IsOfficial = 'T' 
+                                    and CountryCode = 'che' 
+                                    GROUP BY CountryCode 
+                                )
+    ) temp;
+
+-- 7. 국가명이 south korea의 공식 언어는?
+SELECT l.language
+FROM country a, countrylanguage l
+WHERE a.Code = l.CountryCode
+and a.Name = 'south korea'
+and l.isOfficial = 'T';
+
+-- 8. 국가 이름이 bo로 시작하는 국가에 속한 도시의 개수를 출력하시오
+SELECT a.Name, a.Code, count(*)
+FROM country a, city b
+WHERE a.Code = b.CountryCode
+and a.Name like 'bo%'
+GROUP BY a.Name;
+
+-- 9. 국가 이름이 bo로 시작하는 국가에 속한 도시의 개수를 출력하시오. 도시가 없다면 단독 출력.
+SELECT 
+        a.Name,
+        a.Code, 
+        CASE
+            WHEN b.ID is NULL
+            THEN '단독'
+            ELSE count(*)
+        END '도시개수'
+FROM 
+    (
+        SELECT Name, Code
+        FROM country
+        WHERE Name like 'bo%'
+    ) a
+    LEFT OUTER JOIN 
+    city b
+ON a.Code = b.CountryCode
+GROUP BY a.Name;
